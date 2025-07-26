ce1f78f92535cd5f49035b53183181d70718d458
chaxus <nouo18@163.com>
2025-07-26 22:05:01 +0800

    refactor: Clean up formatting and improve error handling in Bulls and Cows game logic, ensuring consistent code style and clarity in the guessing functionality

--- bulls-and-cows/src/lib.rs
+++ bulls-and-cows/src/lib.rs
@@ -6,61 +6,56 @@ declare_id!("3wGUG3qnLtCZFg3ukqeQXNhVYjrr3Jai4RnzEDyqjphc");
 
 #[program]
 pub mod anchor_bac {
-		use super::*;
-		use std::cmp::Ordering;
+    use super::*;
+    use std::cmp::Ordering;
 
-		pub fn initialize(ctx: Context<AccountContext>) -> Result<()> {
-				let guessing_account = &mut ctx.accounts.guessing_account;
-				guessing_account.number = generate_random_number();
-				Ok(())
-		}
-		
-		pub fn guess(ctx: Context<AccountContext>, number: u32) -> Result<()> {
+    pub fn initialize(ctx: Context<AccountContext>) -> Result<()> {
+        let guessing_account = &mut ctx.accounts.guessing_account;
+        guessing_account.number = generate_random_number();
+        Ok(())
+    }
+
+    pub fn guess(ctx: Context<AccountContext>, number: u32) -> Result<()> {
         let guessing_account = &mut ctx.accounts.guessing_account;
         let target = guessing_account.number;
 
         match number.cmp(&target) {
-            Ordering::Less => {
-                return err!(MyError::NumberTooSmall)
-            }
+            Ordering::Less => return err!(MyError::NumberTooSmall),
             Ordering::Greater => {
-                return  err!(MyError::NumberTooLarge);
+                return err!(MyError::NumberTooLarge);
             }
-            Ordering::Equal => {
-                return Ok(())
-            }
+            Ordering::Equal => return Ok(()),
         }
     }
 }
 
 fn generate_random_number() -> u32 {
-		let clock = Clock::get().expect("Failed to get clock");
-		let last_digit = (clock.unix_timestamp % 10) as u8;
-		let result = (last_digit + 1) as u32;
-		result
+    let clock = Clock::get().expect("Failed to get clock");
+    let last_digit = (clock.unix_timestamp % 10) as u8;
+    let result = (last_digit + 1) as u32;
+    result
 }
 
 #[account]
 pub struct GuessingAccount {
-	pub number: u32,
+    pub number: u32,
 }
 
 #[derive(Accounts)]
 pub struct AccountContext<'info> {
-	
-	#[account(
+    #[account(
         init,
         space=32,
         payer=payer,
         seeds = [b"guessing pda"],
         bump
     )]
-	pub guessing_account: Account<'info, GuessingAccount>,
+    pub guessing_account: Account<'info, GuessingAccount>,
 
-	#[account(mut)]
-  pub payer: Signer<'info>,
-	
-	pub system_program: Program<'info, System>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
 }
 
 #[error_code]
@@ -68,5 +63,5 @@ pub enum MyError {
     #[msg("Too small")]
     NumberTooSmall,
     #[msg("Too larget")]
-    NumberTooLarge
+    NumberTooLarge,
 }
